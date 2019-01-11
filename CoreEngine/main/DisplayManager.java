4860fbd319ceecdc7bcf86feee9069f1cef9043a
Finn Kupka <Finn Kupka@MSI.fritz.box>
2019-01-11 16:31:54 +0100

    framerate in the title

--- CoreEngine/main/DisplayManager.java
+++ CoreEngine/main/DisplayManager.java
@@ -3,6 +3,7 @@ package main;
 import java.awt.Toolkit;
 
 import org.lwjgl.LWJGLException;
+import org.lwjgl.Sys;
 import org.lwjgl.opengl.ContextAttribs;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
@@ -12,8 +13,14 @@ public class DisplayManager {
 	
 	private static final int FPS = 60;
 	
+	private static final String TITLE = "Strategiespiel";
+	
 	private static final int WIDTH = Toolkit.getDefaultToolkit().getScreenSize().width;
 	private static final int HEIGHT = Toolkit.getDefaultToolkit().getScreenSize().height;
+	
+	public static float DELTA = 0;
+	private static long lastFrameTime = getTime();
+	private static float framerate = 0;
 	
 	public static void createDisplay() {
 		
@@ -33,12 +40,23 @@ public class DisplayManager {
 		Display.sync(FPS);
 		Display.update();
 		
+		long currentFrameTime = getTime();
+		DELTA = (currentFrameTime - lastFrameTime) / 1000f;
+		lastFrameTime = currentFrameTime;
+		framerate = 1 / DELTA;
+		
+		Display.setTitle(TITLE + "  FPS: " + framerate);
+		
 	}
 	
 	public static void closeDisplay() {
 		
 		Display.destroy();
 		
+	}
+	
+	public static long getTime() {
+		return Sys.getTime() * 1000 / Sys.getTimerResolution();
 	}
 
 }
