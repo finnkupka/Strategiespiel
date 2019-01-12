a2e3bef3438cf0379f083c81e2da357b4c1db6c6
Finn Kupka <Finn Kupka@MSI>
2019-01-12 12:18:07 +0100

    inverted camera movement

--- RenderEngine/camera/Camera.java
+++ RenderEngine/camera/Camera.java
@@ -69,8 +69,8 @@ public class Camera {
 		    } else if (Mouse.getEventButton() == 0) Mouse.setGrabbed(false);
 		}
 		if(Mouse.isGrabbed()) {
-			this.yaw += Mouse.getDX() * sensitivity * DisplayManager.DELTA;
-			this.pitch -= Mouse.getDY() * sensitivity * DisplayManager.DELTA;
+			this.yaw -= Mouse.getDX() * sensitivity * DisplayManager.DELTA;
+			this.pitch += Mouse.getDY() * sensitivity * DisplayManager.DELTA;
 		}
 		//Failsafe:
 		if(Keyboard.isKeyDown(Keyboard.KEY_ESCAPE)) Mouse.setGrabbed(false);
